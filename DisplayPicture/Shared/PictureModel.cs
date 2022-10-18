e4cd3ace2a2188a3a99f2d3c59c758e89826eedd
Kelvin K. Woodman <kwoodman@gmail.com>
2022-10-17 21:09:11 -0400

    Add project files.

---
+++ DisplayPicture/Shared/PictureModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Buffers.Text;
+using System.Xml.Linq;
+
+namespace DisplayPicture.Shared
+{
+    public class PictureModel
+    {
+        public string name { get; set; } = "";
+        public string src { get; set; } = "";
+        public bool shownalready { get; set; } = false;
+        public int sortorder = 0;
+        
+    }
+}
